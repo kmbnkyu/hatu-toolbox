02bb6a0839201870826ac1150f1b7b6bc2f75211
kmbnkyu <kmogksy353688@gmail.com>
2022-06-08 11:26:15 +0000

    memo

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,20 +1,20 @@
 class MemosController < ApplicationController
-  before_action :set_memo, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
-    @memos = Memo.all
+    @pagy, @memos = pagy(current_user.memos.order(id: :desc), items: 25)
   end
 
   def show
   end
 
   def new
-    @memo = Memo.new
+    @memo = current_user.memos.build
   end
 
   def create
-    @memo = Memo.new(message_params)
+    @memo = current_user.memos.build(memo_params)
 
     if @memo.save
       flash[:success] = 'メモが作成されました'
@@ -29,7 +29,7 @@ class MemosController < ApplicationController
   end
 
   def update
-    if @memo.update(message_params)
+    if @memo.update(memo_params)
       flash[:success] = '更新されました'
       redirect_to @memo
     else
@@ -42,16 +42,19 @@ class MemosController < ApplicationController
     @memo.destroy
 
     flash[:success] = '削除されました'
-    redirect_to memos_url
+    redirect_back(fallback_location: root_path)
   end
   
   private
 
-  def set_memo
-    @memo = Memo.find(params[:id])
-  end
-
   def memo_params
-    params.require(:memo).permit(:content)
+    params.require(:memo).permit(:content, :title)
+  end
+  
+  def correct_user
+    @memo = current_user.memos.find_by(id: params[:id])
+    unless @memo
+      redirect_to root_url
+    end
   end
 end
